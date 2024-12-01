66386363315a4a75e6f5fcf14da9577030f4e6e6
wasabina67 <wasabina67@gmail.com>
2024-12-01 13:19:33 +0000

    Update app2.py

--- src/app2.py
+++ src/app2.py
@@ -8,7 +8,23 @@ load_dotenv()
 
 app = Flask(__name__)
 app.secret_key = os.getenv("SECRET_KEY")
-gh_blueprint = OAuth2ConsumerBlueprint()
+gh_blueprint = OAuth2ConsumerBlueprint(
+    "github",
+    __name__,
+    client_id=os.getenv("CLIENT_ID"),
+    client_secret=os.getenv("CLIENT_SECRET"),
+    scope=None,
+    base_url="https://api.github.com/",
+    authorization_url="https://github.com/login/oauth/authorize",
+    token_url="https://github.com/login/oauth/access_token",
+    redirect_url=None,
+    redirect_to=None,
+    login_url=None,
+    authorized_url=None,
+    session_class=None,
+    storage=None,
+    rule_kwargs=None,
+)
 app.register_blueprint(gh_blueprint, url_prefix="/login")
 
 
